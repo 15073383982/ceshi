b0d8ce1e39186e9bd956adde5d3a0050cc19f48a
dell <1771205678@qq.com>
2020-06-18 11:03:45 +0800

    提交测试

--- src/main/java/com/example/swaggerdemo9/Swaggerdemo9Application.java
+++ src/main/java/com/example/swaggerdemo9/Swaggerdemo9Application.java
@@ -9,8 +9,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swaggerdemo9Application {
 
     public static void main(String[] args) {
-
-        System.out.println(11);
+        /*String a=new String("ab");
+        String b=new String("ab");
+        String aa="ab";
+        String bb="ab";
+        System.out.println(aa==bb);
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+        System.out.println(42==42.0);*/
         SpringApplication.run(Swaggerdemo9Application.class, args);
     }
 
