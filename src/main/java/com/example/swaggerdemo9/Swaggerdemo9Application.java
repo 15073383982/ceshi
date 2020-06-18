88f4935b2f53b5f484cee0d0fd89dcacd0cdce5f
dell <1771205678@qq.com>
2020-06-18 10:00:19 +0800

    git提交测试2

--- src/main/java/com/example/swaggerdemo9/Swaggerdemo9Application.java
+++ src/main/java/com/example/swaggerdemo9/Swaggerdemo9Application.java
@@ -9,6 +9,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swaggerdemo9Application {
 
     public static void main(String[] args) {
+
+        System.out.println(11);
         SpringApplication.run(Swaggerdemo9Application.class, args);
     }
 
